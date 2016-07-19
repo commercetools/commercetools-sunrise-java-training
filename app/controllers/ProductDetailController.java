e304ea2ed0e32e1c70bea462a116dcd6845af9a4
Laura Luiz <laura.luiz@commercetools.de>
2016-07-20 00:19:04 +0200

    Add routes and urls to diff params example

--- app/controllers/ProductDetailController.java
+++ app/controllers/ProductDetailController.java
@@ -8,6 +8,10 @@ import javax.inject.Inject;
 
 @RequestScoped
 public class ProductDetailController extends SunriseProductDetailController {
+
+    // http://localhost:9000/en/harris-wharf-blazer-abp3131-cream-M0E20000000DT1S.html
+    // http://localhost:9000/en/test/3a2c70f5-3fad-495b-bdc0-379c6ba7d723-1.html
+
     @Inject
     public void setSuggestionsComponent(final ProductSuggestionsControllerComponent component) {
         registerControllerComponent(component);
