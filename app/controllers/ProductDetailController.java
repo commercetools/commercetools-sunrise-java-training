c154e155a638bc445a5817e331c0dd07e587519d
Laura Luiz <laura.luiz@commercetools.de>
2016-07-20 00:24:43 +0200

    Add TODOs to PDP

--- app/controllers/ProductDetailController.java
+++ app/controllers/ProductDetailController.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 @RequestScoped
 public class ProductDetailController extends SunriseProductDetailController {
 
+    // TODO Show product by product ID and variant ID instead
     // http://localhost:9000/en/harris-wharf-blazer-abp3131-cream-M0E20000000DT1S.html
     // http://localhost:9000/en/test/3a2c70f5-3fad-495b-bdc0-379c6ba7d723-1.html
 
@@ -16,4 +17,8 @@ public class ProductDetailController extends SunriseProductDetailController {
     public void setSuggestionsComponent(final ProductSuggestionsControllerComponent component) {
         registerControllerComponent(component);
     }
+
+    // TODO Show not found product on not found variant
+
+    // TODO Show proper not found message with given slug and SKU
 }
