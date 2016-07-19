dd4bbf15938b8f513aaf4ae0b37ab3a875788d21
Laura Luiz <laura.luiz@commercetools.de>
2016-07-20 00:02:08 +0200

    Provide injected executor

--- app/httpcontextexercise/HttpContextController.java
+++ app/httpcontextexercise/HttpContextController.java
@@ -2,6 +2,7 @@ package httpcontextexercise;
 
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.products.search.ProductProjectionSearch;
+import play.libs.concurrent.HttpExecutionContext;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -13,12 +14,16 @@ import static java.util.Locale.ENGLISH;
 public class HttpContextController extends Controller {
     @Inject
     private SphereClient sphereClient;
+    @Inject
+    private HttpExecutionContext httpExecutionContext;
 
     /*
     given is this normal Play controller
     open http://localhost:9000/exercise/httpcontext
     it will render: [CompletionException: java.lang.RuntimeException: There is no HTTP Context available from here.]
     fix the error
+
+    More info: https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/CompletableFuture.html
      */
     public CompletionStage<Result> show(final String englishSlug) {
         final ProductProjectionSearch request = ProductProjectionSearch.ofCurrent()
