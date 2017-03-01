28c7b44920cbd0ae2727a2d5af182cd5133e59cb
Laura Luiz <laura.luiz@commercetools.de>
2017-03-01 23:35:00 +0100

    Add jsoup exercise

---
+++ app/exercises/addjsoup/AddJsoup.java
@@ -0,0 +1,10 @@
+package exercises.addjsoup;
+
+public class AddJsoup {
+
+//    public static void main(String[] args) {
+//        final Document document = Jsoup.parse("<html><head><title>Sunrise</title></head></html>");
+//        final String title = document.head().select("Hey you got it! :)").text();
+//        System.out.println(title);
+//    }
+}
