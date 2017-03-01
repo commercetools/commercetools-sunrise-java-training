ca9fe9a37522b688d5e0096e9177746436d7ec82
Laura Luiz <laura.luiz@commercetools.de>
2017-03-01 23:38:07 +0100

    Explan Jsoup exercise

--- app/exercises/addjsoup/AddJsoup.java
+++ app/exercises/addjsoup/AddJsoup.java
@@ -1,5 +1,11 @@
 package exercises.addjsoup;
 
+/**
+ * Add a project dependency to Jsoup, the HTML parser for Java.
+ * You can find it in Maven Central Repository: https://mvnrepository.com/artifact/org.jsoup/jsoup/1.10.2
+ *
+ * Once ready, uncomment this code, fix the missing imports and run it.
+ */
 public class AddJsoup {
 
 //    public static void main(String[] args) {
