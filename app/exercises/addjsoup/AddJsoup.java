3235b4e71228d39d057551c1e34bf41ec1dd7448
Laura Luiz <laura.luiz@commercetools.de>
2017-03-25 23:12:33 +0100

    Fix addjsoup exercise

--- app/exercises/addjsoup/AddJsoup.java
+++ app/exercises/addjsoup/AddJsoup.java
@@ -5,12 +5,14 @@ package exercises.addjsoup;
  * You can find it in Maven Central Repository: https://mvnrepository.com/artifact/org.jsoup/jsoup/1.10.2
  *
  * Once ready, uncomment this code, fix the missing imports and run it.
+ * You can run it in the SBT command with:
+ * > runMain exercises.addjsoup.AddJsoup
  */
 public class AddJsoup {
 
-//    public static void main(String[] args) {
-//        final Document document = Jsoup.parse("<html><head><title>Sunrise</title></head></html>");
-//        final String title = document.head().select("Hey you got it! :)").text();
+    public static void main(String[] args) {
+//        final String title = Jsoup.parse("<html><head><title>Hey you got it! :)</title></head></html>")
+//                .head().select("title").text();
 //        System.out.println(title);
-//    }
+    }
 }
